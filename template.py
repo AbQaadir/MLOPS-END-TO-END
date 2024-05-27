20e0f242ed8e6e88772d0b27a1dd2d5f18c66658
AbQaadir <qaadir.inbox@gmail.com>
2024-05-27 13:54:09 +0530

    template.py file updated. :)

--- template.py
+++ template.py
@@ -1,27 +1,38 @@
 import os
 from pathlib import Path
 
+
 def create_files(file_paths):
     for filepath in file_paths:
         path = Path(filepath)
-        
+
         # Create parent directories if they don't exist
         path.parent.mkdir(parents=True, exist_ok=True)
-        
+
         # Create an empty file if it doesn't exist or if it is empty
         if not path.exists() or path.stat().st_size == 0:
             path.touch()
 
-package_name = "mongodb_connect"
+
+package_name = "MLOPS"
 
 list_of_files = [
-    ".github/workflows/ci.yaml",
-    "src/__init__.py",
-    f"src/{package_name}/__init__.py",
-    f"src/{package_name}/mongo_crud.py",
-    "tests/__init__.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
+    ".github/workflows/.gitkeep",
+    "src/init.py",
+    "src/components/init.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/init.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/init.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
+    "tests/unit/init.py",
+    "tests/integration/init.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",

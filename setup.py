909b7067fc527e5b190a1b1ba91ee1db96ee5d59
AbQaadir <qaadir.inbox@gmail.com>
2024-05-29 03:08:03 +0530

    all __init__.py files added for pakage indentificaion purpose :) and setup.py file updated to 'packages = find_packages()'

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ProductionReadyMLOPS',
+    version='0.0.1',
+    install_requires=["scikit-learn","pandas","numpy"],
+    packages=find_packages()
+)
